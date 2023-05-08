b707bc49b9f02cb1b9367e876be33d969ac7d11c
Novik_Vladislav <vladislavnovik95@gmail.com>
2023-05-08 22:05:18 +0300

    hide sanity config on client

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,13 +2,13 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-06'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+  process.env.SANITY_DATASET,
+  'Missing environment variable: SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+  process.env.SANITY_PROJECT_ID,
+  'Missing environment variable: SANITY_PROJECT_ID'
 )
 
 export const useCdn = false
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,5 +1,4 @@
 import { createClient } from 'next-sanity'
-
 import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 const client = createClient({
