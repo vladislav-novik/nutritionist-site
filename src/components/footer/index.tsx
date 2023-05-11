485ddc42126538d1419aaec63d6cf6c70d503bb5
Novik_Vladislav <vladislavnovik95@gmail.com>
2023-05-12 00:29:42 +0300

    style footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,26 +1,37 @@
 import Link from "next/link";
 
 export const Footer = () => {
-    return (
-        <footer className="container mx-auto">
-            <div className="text-center border-t py-8">
-                <h3 className="text-xl">Important Links</h3>
-                <div className="flex items-center justify-center my-4 underline">
-                    <Link href="https://dev.to/jeffsalive" className="px-4">
-                        Dev.to (Jeffrey)
-                    </Link>
-                    <Link href="https://twitter.com/JeffreySunny1" className="px-4">
-                        Twitter
-                    </Link>
-                    <Link href="https://linkedin.com/in/jeffsalive" className="px-4">
-                        LinkedIn
-                    </Link>
-                    <Link href="https://jeffreynwankwo.com" className="px-4">
-                        Website
-                    </Link>
-                </div>
-                <small>Jeffrey &copy; {new Date().getFullYear()}</small>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-white mx-auto overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
+      <nav className="columns-2 -mb-6 space-x-12 sm:flex sm:justify-center" aria-label="Footer">
+        <div className="pb-6">
+          <Link href={'#about'} className="text-gray-600 leading-6 text-sm">About</Link>
+        </div>
+        <div className="pb-6">
+          <Link href={'#about'} className="text-gray-600 leading-6 text-sm">Services</Link>
+        </div>
+        <div className="pb-6">
+          <Link href={'#about'} className="text-gray-600 leading-6 text-sm">Testimonials</Link>
+        </div>
+        <div className="pb-6">
+          <Link href={'#about'} className="text-gray-600 leading-6 text-sm">Blog</Link>
+        </div>
+        <div className="pb-6">
+          <Link href={'#about'} className="text-gray-600 leading-6 text-sm">Contact</Link>
+        </div>
+      </nav>
+      <div className="flex items-center justify-center mt-10 space-x-10">
+        <Link href="https://twitter.com/JeffreySunny1" className="text-gray-400">
+          Twitter
+        </Link>
+        <Link href="https://linkedin.com/in/jeffsalive" className="text-gray-400">
+          LinkedIn
+        </Link>
+        <Link href="https://jeffreynwankwo.com" className="text-gray-400">
+          Website
+        </Link>
+      </div>
+      <p className="text-gray-500 leading-5 text-xs text-center mt-10">© 2020 Your Company, Inc. All rights reserved.</p>
+    </footer>
+  );
 };
