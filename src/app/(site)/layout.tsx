165771c69ef14419b2db5be4f5977f89f12a319e
Novik_Vladislav <vladislavnovik95@gmail.com>
2023-05-28 01:10:43 +0300

    add google analytics

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Open_Sans } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 const openSans = Open_Sans({ subsets: ['latin', 'cyrillic'] })
 
@@ -14,6 +15,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={openSans.className}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KKTXDLKGEV"
+            height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KKTXDLKGEV');
+  `,
+          }}
+        />
         {children}
         <Analytics />
       </body>
