2aa0c40b74ec8eecff64e1525b1742061168e6be
Novik_Vladislav <vladislavnovik95@gmail.com>
2023-11-27 15:51:57 +0200

    try fix build

--- src/app/(site)/(home)/articles/[slug]/page.tsx
+++ src/app/(site)/(home)/articles/[slug]/page.tsx
@@ -2,7 +2,7 @@ import Post from "@/components/post/Post";
 import { getPostBySlug, getPostsSlugs } from "@/sanity/lib/posts";
 import { Metadata } from "next";
 import { generateMetadataForPage } from "@/utils/metadata";
-import { urlForImage } from "@/sanity/lib/image";
+// import { urlForImage } from "@/sanity/lib/image";
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { lang, slug } = params
@@ -31,25 +31,25 @@ export default async function Page({ params }: Props) {
   const { slug } = params
   const post = await getPostBySlug(slug);
 
-  const jsonLd = {
-    '@context': 'https://schema.org',
-    '@type': 'Article',
-    "headline": post.title,
-    "image": [urlForImage(post.mainImage.ref).width(720).height(360).fit("fill").url()],
-    "datePublished": post.publishedAt,
-    "author": [{
-      "@type": "Person",
-      "name": "Marya Novik",
-      "url": "https://example.com/profile/janedoe123"
-    }]
-  };
+  // const jsonLd = {
+  //   '@context': 'https://schema.org',
+  //   '@type': 'Article',
+  //   "headline": post.title,
+  //   "image": [urlForImage(post.mainImage.ref).width(720).height(360).fit("fill").url()],
+  //   "datePublished": post.publishedAt,
+  //   "author": [{
+  //     "@type": "Person",
+  //     "name": "Marya Novik",
+  //     "url": "https://example.com/profile/janedoe123"
+  //   }]
+  // };
 
   return (
     <>
-      <script
+      {/* <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      />
+      /> */}
 
       <div className="sm:px-8">
         <div className="mx-auto max-w-7xl lg:px-8">
