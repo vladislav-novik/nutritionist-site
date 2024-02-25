fe692a60d5013a22c0e2ced4bb79b9183c15245b
Novik_Vladislav <vladislavnovik95@gmail.com>
2024-02-25 17:04:42 +0200

    add vercel speed insights

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Open_Sans } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Script from 'next/script';
 
 const openSans = Open_Sans({ subsets: ['latin', 'cyrillic'] })
@@ -33,6 +34,7 @@ export default function RootLayout({
           }}
         />
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
