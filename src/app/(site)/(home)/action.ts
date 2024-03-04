3c3b4371191e4459ac277c82eb428d2a3c4fff9e
Novik_Vladislav <vladislavnovik95@gmail.com>
2024-03-04 14:05:08 +0200

    add resend to send emails

--- src/app/(site)/(home)/action.ts
+++ src/app/(site)/(home)/action.ts
@@ -1,80 +1,100 @@
 "use server"
 
-import { sendCustomerRequest } from "@/sanity/lib/clientRequests";
-import { CustomerRequest } from "@/types/customerRequest";
+import { sendCustomerRequest } from "@/sanity/lib/clientRequests"
+import { CustomerRequest } from "@/types/customerRequest"
+import { Resend } from 'resend'
+import EmailTemplate from '@/components/email-templates/default';
 
 export async function submitRequest(data: FormData) {
-    console.log('handleSubmit', data)
+  console.log('handleSubmit', data)
 
-    const validationResult = verifyData(data);
+  const validationResult = verifyData(data)
 
-    if (!validationResult.isValid) {
-        return {
-            success: false,
-            status: "Validation error",
-            errors: validationResult.errors,
-        }
+  if (!validationResult.isValid) {
+    return {
+      success: false,
+      status: "Validation error",
+      errors: validationResult.errors,
     }
+  }
 
-    const customerRequest: CustomerRequest = {
-        name: (data.get('name') as string).trim(),
-        feedbackWay: (data.get('feedbackWay') as string).trim(),
-        feedbackWayData: (data.get('feedbackWayData') as string).trim().toLowerCase(),
-        message: (data.get('message') as string).trim(),
-    }
+  const customerRequest: CustomerRequest = {
+    firstName: (data.get('firstName') as string).trim(),
+    howToReach: (data.get('howToReach') as string).trim(),
+    message: (data.get('message') as string).trim(),
+  }
 
-    const result = await sendCustomerRequest(customerRequest);
+  const sendEmailResult = await sendEmail(customerRequest.firstName);
+  console.log('sendEmailResult', sendEmailResult)
 
-    return result._createdAt ? { success: true } : { success: false, errors: [{ field: 'sanity', value: '', isValid: false, validationMessage: 'Error while sending data' }] };
+  const result = await sendCustomerRequest(customerRequest)
+
+  return result._createdAt ? { success: true } : { success: false, errors: [{ field: 'sanity', value: '', isValid: false, validationMessage: 'Error while sending data' }] }
 }
 
 
-const contactFormFields: FormInputValidation[] = [
-    { field: 'name', required: true },
-    { field: 'feedbackWay', required: true },
-    { field: 'feedbackWayData', required: true },
-    { field: 'message', required: true },
-];
+async function sendEmail(firstName: string) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
-function verifyData(formData: FormData): { isValid: boolean, errors: ValidationResult<string>[] } {
-    const validationResut: ValidationResult<string>[] = [];
-
-    contactFormFields.forEach(field => {
-        const value = formData.get(field.field);
-
-        if (field.required && !required<string>(value?.valueOf() as string)) {
-            validationResut.push({
-                field: field.field,
-                value: value?.valueOf() as string,
-                isValid: false,
-                validationMessage: 'Required field',
-            });
-        }
+  try {
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['vladislavnovik95@gmail.com'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName }),
     });
 
-    return validationResut.length === 0 ? { isValid: true, errors: [] } : { isValid: false, errors: validationResut };
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+const contactFormFields: FormInputValidation[] = [
+  { field: 'firstName', required: true },
+  { field: 'howToReach', required: true },
+  { field: 'message', required: true },
+]
+
+function verifyData(formData: FormData): { isValid: boolean, errors: ValidationResult<string>[] } {
+  const validationResut: ValidationResult<string>[] = []
+
+  contactFormFields.forEach(field => {
+    const value = formData.get(field.field)
+
+    if (field.required && !required<string>(value?.valueOf() as string)) {
+      validationResut.push({
+        field: field.field,
+        value: value?.valueOf() as string,
+        isValid: false,
+        validationMessage: 'Required field',
+      })
+    }
+  })
+
+  return validationResut.length === 0 ? { isValid: true, errors: [] } : { isValid: false, errors: validationResut }
 }
 
 function required<T>(value: T): boolean {
-    if (typeof value === 'string') {
-        return value.trim().length > 0;
-    }
+  if (typeof value === 'string') {
+    return value.trim().length > 0
+  }
 
-    if (typeof value === 'number') {
-        return value > 0;
-    }
+  if (typeof value === 'number') {
+    return value > 0
+  }
 
-    return false;
+  return false
 }
 
 type FormInputValidation = {
-    field: string;
-    required: boolean;
+  field: string
+  required: boolean
 }
 
 type ValidationResult<T> = {
-    field: string;
-    value: T;
-    isValid: boolean;
-    validationMessage: string;
+  field: string
+  value: T
+  isValid: boolean
+  validationMessage: string
 }
